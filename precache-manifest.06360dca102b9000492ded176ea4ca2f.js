b3b6976a582813447ce3065af5c041635db4470a
Filianov Pavlo <filianov@Pavlo-MacBook-Pro.local>
2020-04-25 10:52:41 +0300

    Updates

---
+++ precache-manifest.06360dca102b9000492ded176ea4ca2f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe33f6e4b8cac8835c12ab66d4434496",
+    "url": "/goit-react-hw-03-phonebook/index.html"
+  },
+  {
+    "revision": "3d8833c0aede4d70b0dd",
+    "url": "/goit-react-hw-03-phonebook/static/css/main.59a3f64b.chunk.css"
+  },
+  {
+    "revision": "4ead967c7e280ace2d8f",
+    "url": "/goit-react-hw-03-phonebook/static/js/2.b0f3e584.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-03-phonebook/static/js/2.b0f3e584.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3d8833c0aede4d70b0dd",
+    "url": "/goit-react-hw-03-phonebook/static/js/main.5f7a0eaa.chunk.js"
+  },
+  {
+    "revision": "d4e615794c495a35ad0b",
+    "url": "/goit-react-hw-03-phonebook/static/js/runtime-main.98198ba2.js"
+  }
+]);
